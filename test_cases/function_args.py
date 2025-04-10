43b934a2ae633a88ddd689a3d8a9d905769d7ac1
egerc <153100377+egerc@users.noreply.github.com>
2025-04-10 16:39:31 +0200

    some documentation

--- test_cases/function_args.py
+++ test_cases/function_args.py
@@ -1,3 +1,5 @@
+# This dict named function_args is mandatory
+# the two .py files you want to test need to have identically named functions to those in the function_args dict
 function_args = {
     'add': [
         (1, 2), 
